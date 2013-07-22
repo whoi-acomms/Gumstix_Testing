0719b0e974a3acbab1d4ee111969a9f4ca7afaa1
Andrew Beal <noaddress@whoi.edu>
2013-07-22 19:58:11 +0000

    Add changes to mount options.
    git-svn-id: https://dev-acomms.whoi.edu/svn/Platforms/Gumstix_Configuration@10491 f2731822-534f-144a-aff5-02520857a0fd

--- tools/audio_recorder/CONFIGURE.sh
+++ tools/audio_recorder/CONFIGURE.sh
@@ -5,4 +5,4 @@ RECORDINGS_DIR=/home/acomms/recordings
 STORAGE_LOCATION=/dev/sdb1
 
 #Install in FSTAB
-(cat /etc/fstab| grep -v $RECORDINGS_DIR ; echo "${STORAGE_LOCATION} ${RECORDINGS_DIR}  vfat rw        0 0") | uniq > /etc/fstab
+(cat /etc/fstab| grep -v $RECORDINGS_DIR ; echo "${STORAGE_LOCATION} ${RECORDINGS_DIR}  vfat rw,noauto,users,exec,dev        0 0") | uniq > /etc/fstab
