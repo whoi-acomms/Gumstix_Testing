3f3def6a289d30a141361e5e9d612dcf2359a182
Jim Partan <jpartan@whoi.edu>
2015-01-21 16:56:39 +0000

    Some cleanup of the Gumstix_Configuration/tests/ scripts - removing redundant copies.
    git-svn-id: https://dev-acomms.whoi.edu/svn/Platforms/Gumstix_Configuration@17122 f2731822-534f-144a-aff5-02520857a0fd

--- tests/gumstix_testing_suite/OnboardTests/gumstix_sd_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mount /dev/mmcblk1p2 /mnt/
-touch /mnt/test
-echo "helloworld" > /mnt/test 
-cat /mnt/test 
-rm -f /mnt/test 
-umount /mnt
--- tests/gumstix_testing_suite/OnboardTests/gumstix_serial_test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -vf
-
-stty -F $1 115200
-sleep 1
-echo 1234 > $1
-sleep 5
-cat $1
